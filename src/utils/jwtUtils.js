a7cf93d11d2a74d1efdf4d42404504425b2d890c
Jonathan Torres <racknarow1@gmail.com>
2025-11-01 12:52:36 -0500

    mejorar: actualizar la duración del token JWT a 7 días en la función generateJWT

--- src/utils/jwtUtils.js
+++ src/utils/jwtUtils.js
@@ -1,7 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-
 // Función para generar un token JWT (para autenticación)
-export const generateJWT = (payload, expiresIn = '24h') => {
-    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn });
-  };
+export const generateJWT = (payload, expiresIn = '7d') => {
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn });
+};
